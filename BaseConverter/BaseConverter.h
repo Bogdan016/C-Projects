538961d995cedd31ea8c8179b0c533a818756c8c
Bogdan <76945445+Bogdan016@users.noreply.github.com>
2023-08-25 12:53:55 +0300

    Update BaseConverter.h

--- BaseConverter/BaseConverter.h
+++ BaseConverter/BaseConverter.h
@@ -1,5 +1,15 @@
 #pragma once
 
+
+// Function to verify if a number is in binary format
+int isBinary(int n);
+
+// Function to verify if a number is in decimal format
+int isDecimal(int n);
+
+// Function to verify if a string is in octal format
+int isOctal(char *s);
+
 void decimalToBinary(int n);
 
 void decimalToOctal(int n);
@@ -12,4 +22,15 @@ void binaryToOctal(int n);
 
 void binaryToHexa(int n);
 
-void octalTodecimal(int n);
+void octalToDecimal(int n);
+
+void octalToBinary(int n);
+
+void octalToHexa(int n);
+
+void HexaToDecimal(int n);
+
+void HexaToBinary(int n);
+
+void HexaToOctal(int n);
+
