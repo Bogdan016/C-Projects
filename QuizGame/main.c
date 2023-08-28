54200498d2c055a830ef55a15c0297e02b59b45a
Bogdan <76945445+Bogdan016@users.noreply.github.com>
2023-08-28 18:57:13 +0300

    Update main.c

--- QuizGame/main.c
+++ QuizGame/main.c
@@ -17,6 +17,11 @@ int main()
             {"3. All keywords in C are in ____________\n ","a) LowerCase letters\nb) UpperCase letters\nc) CamelCase letters\nd) None of the mentioned\n", 'a', " "},
             {"4. Which of the following is true for variable names in C?\n","a) They can contain alphanumeric characters as well as special characters\nb) It is not an error to declare a variable to be one of the keywords(like goto, static)\nc) Variable names cannot start with a digit\nd) Variable can be of any length\n", 'c', "Explanation: According to the syntax for C variable name, it cannot start with a digit.\n"},
             {"5. Which is valid C expression?\n","a) int my_num = 100,000;\nb) int my_num = 100000;\nc) int my num = 1000;\nd) int $my_num = 10000;\n", 'b', "Explanation: Space, comma and $ cannot be used in a variable name.\n"},
+            {"6. Which of the following cannot be a variable name in C?\n","a) volatile\nb) true\nc) friend\nd) export\n", 'a', "Explanation: volatile is C keyword.\n"},
+            {"7. What is short int in C programming?\n","a) The basic data type of C\nb) Qualifier\nc) Short is the qualifier and int is the basic data type\nd) All of the mentioned", 'c', " "},
+            {"8. Which of the following declaration is not supported by C language?\n","a) String str;\nb) char *str;\nc) float str = 3e2;\nd) Both “String str;” and “float str = 3e2;”\n", 'a', "Explanation: It is legal in Java, but not in C language.\n"},
+            {"9. Which keyword is used to prevent any changes in the variable within a C program?\n","a) immutable\nb) mutable\nc) const\nd) volatile\n", 'c', "Explanation: const is a keyword constant in C program.\n"},
+            {"10. What is the result of logical or relational expression in C?\n","a) True or False\nb) 0 or 1\nc) 0 if an expression is false and any positive number if an expression is true\nd) None of the mentioned\n", 'b', " "}
         };
 
         int nr = sizeof(questions) / sizeof(questions[0]);
